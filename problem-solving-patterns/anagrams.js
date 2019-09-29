281b2e651de43c057f9216e85941e740e0450e56
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-29 15:23:35 -0400

    FEAT(frequencyCounter): attempted solution

---
+++ problem-solving-patterns/anagrams.js
@@ -0,0 +1,24 @@
+const anagrams = function (str1, str2) {
+  let str1chars = {}
+  let str2chars = {}
+
+  for (lett of str1) {
+    if (str1chars[lett]) {
+      str1chars[lett]++
+    } else {
+      str1chars[lett] = 1
+    }
+  }
+
+  for (lett of str2) {
+    if (str2chars[lett]) {
+      str2chars[lett]++
+    } else {
+      str2chars[lett] = 1
+    }
+  }
+
+  for (lett in str1chars) {
+
+  }
+}
