569395a0ed4a4adb6ed3eb2e02863648a6b3cd63
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-01 17:26:41 -0400

    FEAT(countUniqueValues): added colts solution

--- problem-solving-patterns/countUniqueValues.js
+++ problem-solving-patterns/countUniqueValues.js
@@ -24,3 +24,16 @@ function countUniqueValues(array) {
 // return array
 
 console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2]))
+
+// COLT'S SOLUTION
+function coltUniqueValues(arr) {
+  if (arr.length === 0) return 0
+  let i = 0
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++
+      arr[i] = arr[j]
+    }
+  }
+  return i + 1
+}
