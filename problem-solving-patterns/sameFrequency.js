26b5883354141febdac7a5d4efda705308aa80cb
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-01 18:28:13 -0400

    FEAT(sameFrequency): question and examples

---
+++ problem-solving-patterns/sameFrequency.js
@@ -0,0 +1,13 @@
+// Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+// Your solutions must have O(N) time complexity
+
+// Sample inputs:
+// sameFrequency(182, 281) --> true
+// sameFrequency(34, 14) --> false
+// sameFrequency(3589578, 5879385) --> true
+// sameFrequency(22, 222) --> false
+
+function sameFrequency(int1, int2) {
+
+}
