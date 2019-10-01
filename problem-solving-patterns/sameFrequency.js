d37357c2231b6570a3aed62f00cab27e2627a4f3
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-01 18:35:27 -0400

    FEAT(sameFrequency): first solution attempt, how to avoid object.values call

--- problem-solving-patterns/sameFrequency.js
+++ problem-solving-patterns/sameFrequency.js
@@ -9,5 +9,29 @@
 // sameFrequency(22, 222) --> false
 
 function sameFrequency(int1, int2) {
+  let num1 = String(int1)
+  let num2 = String(int2)
+  let digits = {}
 
+  for (let i = 0; i < num1.length; i++) {
+    let digit = num1[i]
+    digits[num1[i]] ? digits[digit] += 1 : digits[digit] = 1
+  }
+
+  for (let i = 0; i < num2.length; i++) {
+    let digit = num2[i]
+    if (!(digits[digit])) {
+      return false
+    } else {
+      digits[digit] -= 1
+    }
+  }
+
+  if (Object.values(digits).includes(1)) return false
+
+  return true
 }
+
+console.log(sameFrequency(182, 281))
+console.log(sameFrequency(34, 14))
+console.log(sameFrequency(222, 22))
