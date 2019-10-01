a80e33cc1c0850c4c1ae82c7860deb45e8eba7dd
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-01 18:39:10 -0400

    FEAT(areThereDuplicates): question and examples

---
+++ problem-solving-patterns/areThereDuplicates.js
@@ -0,0 +1,22 @@
+// implement a function which accepts a variable number of
+//  arguments and checks whether there are any duplicates
+// among the arguments passed in
+//
+// you can solve this using frequency counter pattern OR multiple pointers pattern
+
+// Examples:
+// areThereDuplicates(1, 2, 3) --> false
+// areThereDuplicates(1, 2, 2) --> true
+// areThereDuplicates('a', 'b', 'c', 'a') --> true
+
+// restrictions:
+// Time - O(n)
+// Space - O(n)
+
+// bonus:
+// time - O(n log n)
+// Space - O(1)
+
+function areThereDuplicates() {
+
+}
