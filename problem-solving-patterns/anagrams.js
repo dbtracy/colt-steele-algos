79b68534734a287a0ad8bab0fc9848a74b615414
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-01 16:32:08 -0400

    FEAT(problem solving patterns): anagrams algo potential solution refactored

--- problem-solving-patterns/anagrams.js
+++ problem-solving-patterns/anagrams.js
@@ -1,32 +1,15 @@
 function validAnagram(str1, str2) {
   let str1chars = {}
-  let str2chars = {}
 
   for (lett of str1) {
-    if (str1chars[lett]) {
-      str1chars[lett]++
-    } else {
-      str1chars[lett] = 1
-    }
+    str1chars[lett] ? str1chars[lett]++ : str1chars[lett] = 1
   }
 
   for (lett of str2) {
-    if (str2chars[lett]) {
-      str2chars[lett]++
+    if (!str1chars[lett]) {
+      return false
     } else {
-      str2chars[lett] = 1
-    }
-  }
-
-  for (key in str1chars) {
-    if (!(key in str2chars) || str1chars[key] !== str2chars[key]) {
-      return false
-    }
-  }
-
-  for (key in str2chars) {
-    if (!(key in str1chars) || str2chars[key] !== str1chars[key]) {
-      return false
+      str1chars[lett]--
     }
   }
 
