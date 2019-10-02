a3bb537ac99bbc6c68ac8fdfbfaef2c2e1ff7640
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-02 09:11:49 -0400

    FEAT(isSubsequence): initial setup

---
+++ problem-solving-patterns/isSubsequence.js
@@ -0,0 +1,13 @@
+// write a function isSubsequence that takes in two strings
+// and checks whether the characters in the first string form
+// a subsequence of the characters in the second string. In
+// other words, the function should check whether the
+// characters in the first string appear somewhere in the second string
+
+// must have at least:
+// Time complexity: O(n + m)
+// Space complexity: O(1)
+
+function isSubsequence(str1, str2) {
+
+}
