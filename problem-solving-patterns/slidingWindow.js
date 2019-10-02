394049dbe222344583b81f4cb9ca368675c8d169
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-02 10:04:12 -0400

    FEAT(minSubArrayLen): initial setup

--- problem-solving-patterns/slidingWindow.js
+++ problem-solving-patterns/slidingWindow.js
@@ -10,13 +10,13 @@
 
 // Examples:
 
-// minSubArrayLen([2,3,1,2,4,3], 7)
-// minSubArrayLen([2,1,6,5,4], 9)
-// minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)
-// minSubArrayLen([1,4,16,22,5,7,8,9,10], 39)
-// minSubArrayLen([1,4,16,22,5,7,8,9,10], 55)
-// minSubArrayLen([4,3,3,8,1,2,3], 11)
-// minSubArrayLen([1,4,16,22,5,7,8,9,10], 95)
+// minSubArrayLen([2,3,1,2,4,3], 7) --> 2
+// minSubArrayLen([2,1,6,5,4], 9) --> 2
+// minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) --> 1
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 39) --> 3
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 55) --> 5
+// minSubArrayLen([4,3,3,8,1,2,3], 11) --> 2
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 95) --> 0
 
 // Time: O(n)
 // Space: O(1)
