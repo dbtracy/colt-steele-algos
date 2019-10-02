da2ac3ca0e4e9bb2a31cf8bb62b7b45daa319cbf
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-02 10:02:58 -0400

    FEAT(minSubArrayLen): initial setup

---
+++ problem-solving-patterns/slidingWindow.js
@@ -0,0 +1,26 @@
+// write a function called minSubArrayLen which
+// accepts two parameters - an array of postiive
+// integers and a positive integer
+
+// This funciton should return the minimal length
+// of a contiguous subarray of which the sum is
+// greater than or equal to the integer passed to
+// the function. If there isn't one, return 0
+// instead.
+
+// Examples:
+
+// minSubArrayLen([2,3,1,2,4,3], 7)
+// minSubArrayLen([2,1,6,5,4], 9)
+// minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 39)
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 55)
+// minSubArrayLen([4,3,3,8,1,2,3], 11)
+// minSubArrayLen([1,4,16,22,5,7,8,9,10], 95)
+
+// Time: O(n)
+// Space: O(1)
+
+function minSubArrayLen(array, int) {
+
+}
