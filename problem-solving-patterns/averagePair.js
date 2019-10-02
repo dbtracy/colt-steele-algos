e3c80782465031a112e50315c0c54eff3d0c0ab0
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-02 09:08:14 -0400

    FEAT(averagePair): isolution

--- problem-solving-patterns/averagePair.js
+++ problem-solving-patterns/averagePair.js
@@ -10,7 +10,34 @@
 
 // Examples:
 
-// averagePair([1,2,3],2,5)
+// averagePair([1,2,3],2.5)
 // averagePair([1,3,3,5,6,7,10,12,19],8)
 // averagePair([-1,0,3,4,5,6],4.1)
 // averagePair([],4)
+
+function averagePair(array, average) {
+  let left = 0
+  let right = array.length - 1
+  let sum
+  let sumAv
+
+  while (left < right) {
+    sum = array[left] + array[right]
+    sumAv = sum / 2
+    console.log(array[left], array[right], sumAv)
+    if (sumAv === average) {
+      return true
+    } else if (sumAv < average) {
+      left++
+    } else (
+      right--
+    )
+  }
+
+  return false
+}
+
+console.log(averagePair([1, 2, 3], 2.5))
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8))
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1))
+console.log(averagePair([], 4))
