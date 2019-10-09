dd5ceab8838cd4a2a11065e39e011cc55e7f567e
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-09 14:05:00 -0400

    FEAT(power.js): setup

---
+++ recursion/power.js
@@ -0,0 +1,20 @@
+// Write a function called power which accepts a base and an
+// exponent. The function should return the power of the base
+// to the exponent. This function should mimic the
+// funcitonality of Math.pow() - don't worry about negative
+// bases and exponents.
+
+// Examples:
+
+// power(2,0) --> 1
+// power(2,2) --> 4
+// power(2,4) --> 16
+
+function power(base, exponent) {
+
+}
+
+
+// console.log(power(2,0))
+// console.log(power(2,2))
+// console.log(power(2,4))
