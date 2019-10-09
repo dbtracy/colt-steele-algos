f2599c910ae32572854d0d32c213c6db7e60222a
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-09 14:12:56 -0400

    FEAT(productOfArray.js): setup

---
+++ recursion/productOfArray.js
@@ -0,0 +1,9 @@
+// Write a function which takes in an array of numbers and
+// returns the product of them all
+
+function productOfArray() {
+
+}
+
+// console.log([1,2,3]) // 6
+// console.log([1,2,3,10]) // 60
