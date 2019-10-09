d8a66b6e7f69bb23c3e9c1903bb35d770a6efae5
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-09 11:02:54 -0400

    FEAT(findLongestSubstring): setup

---
+++ problem-solving-patterns/findLongestSubstring.js
@@ -0,0 +1,33 @@
+// Write a function called findLongestSubstring which accepts a string and
+// returns the length of the longest substring with all distinct
+// characters.
+
+// Examples
+// findLongestSubstring('') --> 0
+// findLongestSubstring('rithmschool') --> 7
+// findLongestSubstring('thisisawesome') --> 6
+// findLongestSubstring('thecatinthehat')
+// findLongestSubstring('bbbbbb')
+// findLongestSubstring('longestsubstring')
+// findLongestSubstring('thisishowwedoit')
+
+function findLongestSubstring(string) {
+
+}
+
+
+
+
+
+
+
+
+
+
+// console.log(findLongestSubstring(''))
+// console.log(findLongestSubstring('rithmschool'))
+// console.log(findLongestSubstring('thisisawesome'))
+// console.log(findLongestSubstring('thecatinthehat'))
+// console.log(findLongestSubstring('bbbbbb'))
+// console.log(findLongestSubstring('longestsubstring'))
+// console.log(findLongestSubstring('thisishowwedoit'))
