ab49209b54f25a552696ebb7dd366da275da643f
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-09 14:08:46 -0400

    FEAT(power.js): solution

--- recursion/power.js
+++ recursion/power.js
@@ -11,10 +11,11 @@
 // power(2,4) --> 16
 
 function power(base, exponent) {
-
+  if (exponent === 0) return 1
+  else return base * power(base, exponent - 1)
 }
 
 
-// console.log(power(2,0))
-// console.log(power(2,2))
-// console.log(power(2,4))
+console.log(power(2, 0))
+console.log(power(2, 2))
+console.log(power(2, 4))
