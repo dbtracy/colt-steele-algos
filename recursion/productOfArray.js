cc2d03569f02de562857402047c697dccc4cda5a
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-09 14:19:35 -0400

    FEAT(productOfArray.js): solution

--- recursion/productOfArray.js
+++ recursion/productOfArray.js
@@ -1,9 +1,13 @@
 // Write a function which takes in an array of numbers and
 // returns the product of them all
 
-function productOfArray() {
-
+function productOfArray(arr) {
+  if (arr.length === 1) {
+    return arr[0]
+  }
+  let first = arr[0]
+  return first * productOfArray(arr.slice(1))
 }
 
-// console.log([1,2,3]) // 6
-// console.log([1,2,3,10]) // 60
+console.log(productOfArray([1, 2, 3])) // 6
+console.log(productOfArray([1, 2, 3, 10])) // 60
