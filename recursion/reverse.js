13e6ee95d80ab9c11dcbe8485e8a290532a4d11b
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-14 15:39:47 -0400

    FEAT(recursion): reverse.js setup

---
+++ recursion/reverse.js
@@ -0,0 +1,12 @@
+// Write recursive function called reverse which accepts
+// a string and returns a new string in reverse
+
+function reverse(string) {
+  let result = ''
+  if (string.length === 1) {
+    result += string[0]
+  }
+}
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
