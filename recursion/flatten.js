5d1c00ed04d01c6c1e8431dc2f4f434dc7630478
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-17 09:11:16 -0400

    FEAT(recursion): flatten solution

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -2,10 +2,18 @@
 // with all values flattened
 
 function flatten(array) {
-
+  let result = []
+  array.forEach(el => {
+    if (typeof el === "number") {
+      result.push(el)
+    } else {
+      result = [...result, ...flatten(el)]
+    }
+  })
+  return result
 }
 
-// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
-// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
-// flatten([[1],[2],[3]]) // [1,2,3]
-// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
+console.log(flatten([1, 2, 3, [4, 5]])) // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])) // [1, 2, 3, 4, 5]
+console.log(flatten([[1], [2], [3]])) // [1,2,3]) // [1,2,3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) // [1,2,3]
