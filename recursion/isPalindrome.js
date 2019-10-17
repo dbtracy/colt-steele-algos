d7454f93530598c6cceaa33e14f5056ca9e198ef
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-17 08:48:01 -0400

    FEAT(recursion): isPalindrome setup

---
+++ recursion/isPalindrome.js
@@ -0,0 +1,13 @@
+// Write a function called isPalidrome which returns true
+// if the string passed to it is a palindrome, otherwise
+// return false
+
+function isPalidrome(string) {
+
+}
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
