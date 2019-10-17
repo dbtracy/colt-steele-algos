9e076533ad9bb60fa32b18f84bea82cb938706dd
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-17 08:54:15 -0400

    FEAT(recursion): isPalindrome solution

--- recursion/isPalindrome.js
+++ recursion/isPalindrome.js
@@ -1,13 +1,24 @@
-// Write a function called isPalidrome which returns true
+// Write a recursive function called isPalidrome which returns true
 // if the string passed to it is a palindrome, otherwise
 // return false
 
-function isPalidrome(string) {
-
+function isPalindrome(string) {
+  if (string.length <= 1) {
+    return true
+  } else {
+    let first = string[0]
+    let last = string[string.length - 1]
+    if (first === last) {
+      return isPalindrome(string.slice(1, string.length - 1))
+    } else return false
+  }
 }
 
-// isPalindrome('awesome') // false
-// isPalindrome('foobar') // false
-// isPalindrome('tacocat') // true
-// isPalindrome('amanaplanacanalpanama') // true
-// isPalindrome('amanaplanacanalpandemonium') // false
+console.log(isPalindrome('awesome')) // false
+console.log(isPalindrome('foobar')) // false
+console.log(isPalindrome('tacocat')) // true
+console.log(isPalindrome('amanaplanacanalpanama')) // true
+console.log(isPalindrome('amanaplanacanalpandemonium')) // false
+console.log(isPalindrome('a'))
+console.log(isPalindrome(''))
+console.log(isPalindrome('aa'))
