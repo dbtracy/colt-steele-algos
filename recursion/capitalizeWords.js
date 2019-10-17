1509b1157a5250c16701f64a23460e3ec0b3e00d
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-17 09:35:01 -0400

    FEAT(recursion): capitalizeWords setup

---
+++ recursion/capitalizeWords.js
@@ -0,0 +1,10 @@
+// given an array of words, return new array
+// containing each word capitalized
+
+
+function capitalizeWords() {
+
+}
+
+// let words = ['i', 'am', 'learning', 'recursion'];
+// capitalizeWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
