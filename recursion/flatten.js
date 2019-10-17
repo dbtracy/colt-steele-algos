62d8d8c7b9b9ba69ddfe22ad62ed07fa0ca254a7
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-17 09:05:23 -0400

    FEAT(recursion): flatten setup

---
+++ recursion/flatten.js
@@ -0,0 +1,11 @@
+// accepts array of arrays and returns new array
+// with all values flattened
+
+function flatten(array) {
+
+}
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
