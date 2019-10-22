3aa2ef5842c51ffaae6776c081caf03d837c3486
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-22 15:45:36 -0400

    FEAT(mergeSort): solution

---
+++ sorting-algos/mergeSort.js
@@ -0,0 +1,28 @@
+function mergingArrays(arr1, arr2) {
+  let result = []
+  let i = 0
+  let j = 0
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i])
+      i++
+    } else {
+      result.push(arr2[j])
+      j++
+    }
+  }
+  if (i < arr1.length) result = [...result, ...arr1.slice(i)]
+  if (j < arr2.length) result = [...result, ...arr2.slice(j)]
+  return result
+}
+
+function mergeSort(arr) {
+  if (arr.length < 2) return arr
+  let front = arr.slice(0, Math.floor(arr.length / 2))
+  let back = arr.slice(Math.floor(arr.length / 2))
+
+  return mergingArrays(mergeSort(front), mergeSort(back))
+}
+
+console.log(mergeSort([4, 3, 6, 7, 88, 3, 452, 12, 0]))
+console.log(mergeSort([5, 4, 3, 2, 1]))
