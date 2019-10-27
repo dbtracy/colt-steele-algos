b400ae0101123726ab02afa5e6b539b5b89faa2d
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-27 18:30:23 -0400

    FEAT(DLL): push, pop, shift, unshift methods

--- doubly-linked-lists/class-construction.js
+++ doubly-linked-lists/class-construction.js
@@ -12,4 +12,58 @@ class DoublyLinkedList {
     this.tail = null
     this.length = 0
   }
+  push(val) {
+    let newbie = new Node(val)
+    if (!this.head) {
+      this.head = newbie
+      this.tail = newbie
+    } else {
+      newbie.prev = this.tail
+      this.tail.next = newbie
+      this.tail = newbie
+    }
+    this.length++
+    return this
+  }
+  pop() {
+    if (!this.head) return undefined
+    let popped = this.tail
+    else if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = popped.prev
+      this.tail.next = null
+      popped.prev = null
+    }
+    this.length--
+    return popped
+  }
+  shift() {
+    if (!this.head) return undefined
+    let shifted = this.head
+    else if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.head = shifted.next
+      this.head.prev = null
+      shifted.next = null
+    }
+    this.length--
+    return shifted
+  }
+  unshift(val) {
+    let newbie = new Node(val)
+    if (!this.head) {
+      this.head = newbie
+      this.tail = newbie
+    } else {
+      newbie.next = this.head
+      this.head.prev = newbie
+      this.head = newbie
+    }
+    length++
+    return this
+  }
 }
