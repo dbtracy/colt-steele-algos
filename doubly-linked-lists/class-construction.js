952bee9cca1dc24859dda0a68139d5694595965f
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-28 11:26:15 -0400

    FEAT(DLL): remove method and space/time complexity

--- doubly-linked-lists/class-construction.js
+++ doubly-linked-lists/class-construction.js
@@ -109,23 +109,62 @@ class DoublyLinkedList {
     this.length++
     return true
   }
+  remove(idx) {
+    if (idx < 0 || idx > this.length) return undefined
+    if (idx === 0) return this.shift()
+    if (idx === this.length - 1) return this.pop()
+
+    let removed = this.get(idx)
+    let left = this.get(idx - 1)
+    let right = this.get(idx + 1)
+
+    left.next = right
+    right.prev = left
+    removed.next = null
+    removed.prev = null
+    this.length--
+
+    return removed
+  }
+  print() {
+    let result = []
+    if (!this.head) return result
+
+    let curr = this.head
+    for (let i = 0; i < this.length; i++) {
+      result.push(curr.val)
+      curr = curr.next
+    }
+    console.log('RESULT:', result)
+  }
 }
 
+// SPACE AND TIME COMPLEXITY
+// Insertion: O(1)
+// Removal: O(1)
+// Searching: O(n)
+// Access: O(n)
+
 let list = new DoublyLinkedList()
+list.push(0)
 list.push(1)
 list.push(2)
 list.push(3)
-// list.push(4)
-// list.push(5)
-// list.push(6)
-// list.push(7)
-// list.push(8)
-// list.push(9)
-// list.push(10)
-// list.push(11)
-// list.push(12)
-// list.push(13)
-// list.push(14)
-// console.log(list)
+list.push(4)
+list.push(5)
+list.push(6)
+list.push(7)
+list.push(8)
+list.push(9)
+list.push(10)
+list.push(11)
+list.push(12)
+list.push(13)
+list.push(14)
+list.print()
 list.insert(1, 'YO')
-console.log(list)
+list.print()
+list.remove(1)
+list.print()
+list.insert(6, 'IM HERE')
+list.print()
