55cdac6717fcdc45a6a34f203b8b3d2f4557550a
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-31 16:38:06 -0400

    FEAT(BST): class construction, insert method

---
+++ trees/class-construction.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null
+  }
+  insert(val) {
+    const newbie = new Node(val)
+    if (!this.root) {
+      this.root = newbie
+      console.log(this)
+      return this
+    } else {
+      let curr = this.root
+      while (curr) {
+        if (val < curr.val) {
+          if (curr.left === null) {
+            curr.left = newbie
+            console.log(this)
+            return this
+          } else curr = curr.left
+        } else {
+          if (curr.right === null) {
+            curr.right = newbie
+            console.log(this)
+            return this
+          } else curr = curr.right
+        }
+      }
+    }
+    console.log(this)
+    return this
+  }
+}
+
+const bst = new BinarySearchTree()
+
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
