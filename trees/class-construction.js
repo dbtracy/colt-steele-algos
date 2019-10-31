64108329fe615570ecf70e10446e68c944b7f93a
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-31 16:52:13 -0400

    FEAT(BST): find method

--- trees/class-construction.js
+++ trees/class-construction.js
@@ -14,7 +14,7 @@ class BinarySearchTree {
     const newbie = new Node(val)
     if (!this.root) {
       this.root = newbie
-      console.log(this)
+      // console.log(this)
       return this
     } else {
       let curr = this.root
@@ -23,18 +23,27 @@ class BinarySearchTree {
         if (val < curr.val) {
           if (curr.left === null) {
             curr.left = newbie
-            console.log(this)
+            // console.log(this)
             return this
           } else curr = curr.left
         } else {
           if (curr.right === null) {
             curr.right = newbie
-            console.log(this)
+            // console.log(this)
             return this
           } else curr = curr.right
         }
       }
     }
+  }
+  find(val) {
+    let curr = this.root
+    while (curr) {
+      if (val === curr.val) return true
+      else if (val < curr.val) curr = curr.left
+      else curr = curr.right
+    }
+    return false
   }
 }
 
@@ -44,3 +53,6 @@ const bst = new BinarySearchTree()
 bst.insert(10)
 bst.insert(5)
 bst.insert(15)
+
+console.log(bst.find(15))
+console.log(bst.find(12))
