d0717ad4a09366d2044ef5f6d7d132cdc580f090
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-11-01 15:12:21 -0400

    FEAT(graphs): removeEdge method

--- graphs/class-construction.js
+++ graphs/class-construction.js
@@ -9,6 +9,12 @@ class Graph {
     this.adjacencyList[v1].push(v2)
     this.adjacencyList[v2].push(v1)
   }
+  removeEdge(v1, v2) {
+    let v1List = this.adjacencyList[v1]
+    let v2List = this.adjacencyList[v2]
+    this.adjacencyList[v1] = v1List.filter(el => el !== v2)
+    this.adjacencyList[v2] = v2List.filter(el => el !== v1)
+  }
 }
 
 g = new Graph()
@@ -19,4 +25,8 @@ console.log(g)
 g.addEdge('Tokyo', 'San Francisco')
 console.log(g)
 g.addEdge('Dallas', 'San Francisco')
+console.log(g)
+g.removeEdge('Dallas', 'San Francisco')
+console.log(g)
+g.removeEdge('Tokyo', 'San Francisco')
 console.log(g)
