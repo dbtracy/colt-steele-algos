6fae8559ee2ff30a2ae7a49964d8d1814c93e62d
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-11-01 15:03:47 -0400

    FEAT(graphs): addEdge method

--- graphs/class-construction.js
+++ graphs/class-construction.js
@@ -5,11 +5,18 @@ class Graph {
   addVertex(name) {
     if (!this.adjacencyList[name]) this.adjacencyList[name] = []
   }
-  addEdge() {
-
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2)
+    this.adjacencyList[v2].push(v1)
   }
 }
 
 g = new Graph()
 g.addVertex('Tokyo')
+g.addVertex('San Francisco')
+g.addVertex('Dallas')
+console.log(g)
+g.addEdge('Tokyo', 'San Francisco')
+console.log(g)
+g.addEdge('Dallas', 'San Francisco')
 console.log(g)
