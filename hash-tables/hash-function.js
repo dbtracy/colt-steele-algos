004af8d023d4a49b385d2f0ad52393ef92f64bc8
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-12-03 14:27:54 -0800

    FEAT(hash): improved hash function

--- hash-tables/hash-function.js
+++ hash-tables/hash-function.js
@@ -1,8 +1,23 @@
+/* initial hash */
+
 function hash() {
   let total = 0
   for (let char of key) {
-    let value = charCodeAt(0) - 96
+    let value = char.charCodeAt(0) - 96
     total = (total + value) % arrayLen
+  }
+  return total
+}
+
+/* refining hash */
+
+function improvedHash(key, arrayLen) {
+  let total = 0
+  let WEIRD_PRIME = 31
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i]
+    let value = char.charCodeAt(0) - 96
+    total = (total * WEIRD_PRIME + value) % arrayLen
   }
   return total
 }
