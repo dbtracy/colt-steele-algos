c64d566239f38983c10d4295bd242bec4bf19f83
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-12-03 14:44:24 -0800

    FEAT(hash): set method

--- hash-tables/hash-function.js
+++ hash-tables/hash-function.js
@@ -21,3 +21,30 @@ function improvedHash(key, arrayLen) {
   }
   return total
 }
+
+/* class definition */
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size)
+  }
+
+  _hash(key) {
+    let total = 0
+    let WEIRD_PRIME = 31
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i]
+      let value = char.charCodeAt(0) - 96
+      total = (total * WEIRD_PRIME + value) % arrayLen
+    }
+    return total
+  }
+  set(key, value) {
+    let hashed = this._hash(key)
+    if (!this.keyMap[hashed]) {
+      this.keyMap[hashed] = [[key, value]]
+    } else {
+      this.keyMap[hashed].push([key, value])
+    }
+  }
+}
