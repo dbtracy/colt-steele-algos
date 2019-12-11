b88407045b5aa2f5bfdacf9b7d2961a741d3dd36
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-12-10 22:41:08 -0800

    FEAT(hash): commented improvedHash

--- hash-tables/hash-function.js
+++ hash-tables/hash-function.js
@@ -11,16 +11,16 @@
 
 /* refining hash */
 
-function improvedHash(key, arrayLen) {
-  let total = 0
-  let WEIRD_PRIME = 31
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i]
-    let value = char.charCodeAt(0) - 96
-    total = (total * WEIRD_PRIME + value) % arrayLen
-  }
-  return total
-}
+// function improvedHash(key, arrayLen) {
+//   let total = 0
+//   let WEIRD_PRIME = 31
+//   for (let i = 0; i < Math.min(key.length, 100); i++) {
+//     let char = key[i]
+//     let value = char.charCodeAt(0) - 96
+//     total = (total * WEIRD_PRIME + value) % arrayLen
+//   }
+//   return total
+// }
 
 /* class definition */
 
