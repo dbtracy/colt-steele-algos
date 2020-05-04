d5d6c6dd63272f0caf2aae03d13812b890af725e
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2020-05-04 13:13:51 -0400

    lru cache

--- dynamic-programming/coinChange.js
+++ dynamic-programming/coinChange.js
@@ -1,3 +1,31 @@
-function coinChange(denoms, n) {
+/**
+ * write a function that determines the number of ways you can obtain the value
+ * from the given collection of denominations (number of different ways you can make change)
+ */
 
+const denominations = [1, 5, 10, 25]
+
+function coinChange(denoms, amt, memo) {
+  if (amt < 5) return 1;
+
+  let dynArr = [0, 1, 1, 1, 1, 1];
+
+  for (let coin of denoms) {
+    if (amt >= coin) {
+
+    }
+  }
 }
+
+
+console.log(coinChange(denominations, 1)) // 1
+console.log(coinChange(denominations, 2)) // 1
+console.log(coinChange(denominations, 5)) // 2
+console.log(coinChange(denominations, 10)) // 4
+console.log(coinChange(denominations, 15)) // ??
+console.log(coinChange(denominations, 25)) // 13
+console.log(coinChange(denominations, 45))
+console.log(coinChange(denominations, 100))
+console.log(coinChange(denominations, 145))
+console.log(coinChange(denominations, 1451))
+console.log(coinChange(denominations, 14511))
---
+++ misc-practice/lru_cache.js
@@ -0,0 +1,79 @@
+class Node {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  unshift(key, val) { // add to front of line (most recently used)
+    const newNode = new Node(key, val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return newNode;
+  }
+  pop() { // remove from back of line (least recently used)
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      this.tail.next.prev = null;
+      this.length--;
+    }
+  }
+  remove(node) { // cut node pointers
+    if (!node.next) node.next.prev = node.prev;
+    else this.tail = node.prev;
+    if (!node.prev) node.prev.next = node.next;
+    else this.head = node.next;
+    node.prev = null;
+    node.next = null;
+    this.length--;
+    return node;
+  }
+}
+
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.recently = new DoublyLinkedList();
+    this.cache = {};
+  }
+  get(key) {
+    if (!this.cache[key]) return -1
+    this.recently.remove(this.cache[key])
+    this.cache[key] = this.recently.unshift(key, this.cache[key].val)
+    return this.cache[key].val
+  }
+  put(key, value) {
+    if (this.cache[key]) {
+      this.recently.remove(this.cache[key])
+      this.cache[key] = this.recently.unshift(key, value)
+    } else {
+      if (this.recently.length < this.capacity) {
+        this.cache[key] = this.recently.unshift(key, value)
+      } else {
+        delete this.cache[this.recently.tail.key]
+        this.recently.pop()
+        this.cache[key] = this.recently.unshift(key, value)
+      }
+    }
+  }
+}
